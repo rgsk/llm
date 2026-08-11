d0c71f9f9d1d6d5410370f8b699af8e6ddb0e842
rgsk <guptarahul70322@gmail.com>
2026-08-12 00:50:39 +0530

    single head

--- src/main.py
+++ src/main.py
@@ -13,6 +13,7 @@ DATA = ROOT / "data" / "input.txt"
 text = DATA.read_text(encoding="utf-8")
 itoc = sorted(set(text))
 ctoi = {c: i for i, c in enumerate(itoc)}
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
 def encode(s: str):
@@ -27,8 +28,8 @@ data = torch.tensor(encode(text))
 n = int(0.9 * len(data))
 train_data, val_data = data[:n], data[n:]
 
-block_size = 8  # t
-batch_size = 32  # b
+block_size = 8  # T
+batch_size = 32  # B
 
 
 @jaxtyped(typechecker=beartype)
@@ -39,16 +40,46 @@ def get_batch(
     ix = torch.randint(len(d) - block_size, (batch_size,))
     x = torch.stack([d[i : i + block_size] for i in ix])
     y = torch.stack([d[i + 1 : i + 1 + block_size] for i in ix])
-    return x, y
+    return x.to(device), y.to(device)
 
 
-vocab_size = len(itoc)  # v
+vocab_size = len(itoc)  # V
+n_embed = 32  # E
+
+
+class Head(nn.Module):
+    tril: Tensor
+
+    def __init__(self, head_size: int):
+        super().__init__()
+        self.head_size = head_size
+        self.key = nn.Linear(n_embed, head_size, bias=False)
+        self.query = nn.Linear(n_embed, head_size, bias=False)
+        self.value = nn.Linear(n_embed, head_size, bias=False)
+        self.register_buffer("tril", torch.tril(torch.ones(block_size, block_size)))
+
+    @jaxtyped(typechecker=beartype)
+    def forward(self, x: Float[Tensor, "b t e"]):
+        B, T, E = x.shape
+        q = self.query(x)  # [B, T, hs]
+        k = self.key(x)  # [B, T, hs]
+        scores = (
+            q @ rearrange(k, "b t hs -> b hs t") * self.head_size**-0.5
+        )  # [B, T, T]
+        scores = scores.masked_fill(self.tril[:T, :T] == 0, float("-inf"))
+        w = F.softmax(scores, dim=-1)  # [B, T, T]
+        v = self.value(x)  # [B, T, hs]
+        out = w @ v  # [B, T, hs]
+        return out
 
 
 class BigramLM(nn.Module):
     def __init__(self):
         super().__init__()
-        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embed)
+        self.position_embedding_table = nn.Embedding(block_size, n_embed)
+        self.sa_head = Head(n_embed)
+        self.lm_head = nn.Linear(n_embed, vocab_size)
 
     @jaxtyped(typechecker=beartype)
     def forward(
@@ -56,7 +87,13 @@ class BigramLM(nn.Module):
         idx: Int[Tensor, "b t"],
         targets: Int[Tensor, "b t"] | None = None,
     ) -> tuple[Float[Tensor, "b t v"], Float[Tensor, ""] | None]:
-        logits = self.token_embedding_table(idx)
+        B, T = idx.shape
+        tok_embed = self.token_embedding_table(idx)  # [B, T, E]
+        pos = torch.arange(T, device=idx.device)
+        pos_embed = self.position_embedding_table(pos)  # [T, E]
+        x = tok_embed + pos_embed  # [B, T, E]
+        x = self.sa_head(x)  # [B, T, E]
+        logits = self.lm_head(x)  # [B, T, V]
         loss = None
         if targets is not None:
             loss = F.cross_entropy(
@@ -72,13 +109,14 @@ class BigramLM(nn.Module):
     ) -> Int[Tensor, "b t_out"]:
         for _ in range(max_new_tokens):
             logits, _ = self(idx[:, -block_size:])
-            probs = F.softmax(logits[:, -1, :], dim=-1)  # (b, v)
-            nxt = torch.multinomial(probs, num_samples=1)  # (b, 1)
+            probs = F.softmax(logits[:, -1, :], dim=-1)  # [B, V]
+            nxt = torch.multinomial(probs, num_samples=1)  # [B, 1]
             idx = torch.cat([idx, nxt], dim=1)
         return idx
 
 
 model = BigramLM()
+model.to(device)
 lr = 1e-2
 opt = torch.optim.AdamW(model.parameters(), lr=lr)
 max_steps = 1000
@@ -91,6 +129,6 @@ for it in range(max_steps):
     if it % 100 == 0 or it == max_steps - 1:
         print(f"it: {it}, loss: {loss.item()}")
 
-start = torch.tensor([encode("\n")])
+start = torch.tensor([encode("\n")], device=device)
 sample = decode(model.generate(start, max_new_tokens=500)[0].tolist())
 print(sample)
